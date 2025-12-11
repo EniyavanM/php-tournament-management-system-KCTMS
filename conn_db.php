58f59c2667f58191d06c935db044bdae6f7fc339
EniyavanM <eniyavan7778@gmail.com>
2025-12-12 00:34:46 +0530

    Update conn_db.php

--- conn_db.php
+++ conn_db.php
@@ -5,9 +5,9 @@ mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
 $db_host = "127.0.0.1";
 $db_user = "root";
-$db_pass = "Root@123";   // set to your MySQL root password
+$db_pass = "";   // set to your MySQL root password
 $db_name = "kctms";
-$db_port = 3307;
+$db_port = 3306;
 
 $mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name, $db_port);
 $mysqli->set_charset("utf8mb4");
